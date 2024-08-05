984453508a0768f602b87c2835ea2d9471fe88f7
matrixgitlab <150345998+matrixgitlab@users.noreply.github.com>
2024-08-05 15:32:16 +0200

    Update affiliatelink.js

--- affiliatelink.js
+++ affiliatelink.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 async function scrapePage(aliexpressUrl) {
-  const browser = await puppeteer.launch({ headless: true});
+  const browser = await puppeteer.launch({ headless: true,
+                                         executablePath: '/opt/render/.cache/puppeteer/chrome/linux-126.0.6478.126/chrome-linux64/chrome',
+                                         cacheDir: '/opt/render/.cache/puppeteer'});
   const page = await browser.newPage();
 
   // Définir un cookie spécifique
